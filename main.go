b606dc424669fbb85d5851cf341488e641f33c81
Vinnie Marone <vinnie.marone35@gmail.com>
2022-08-08 17:47:22 -0400

    Add handler from gin

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/Porthorian/urling.io/handler"
+	"github.com/Porthorian/urling.io/store"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,9 +11,20 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Urlame!",
+			"message": "Shortmy cuck",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
