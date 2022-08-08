c1bfed5b0dc3762dbf9d2d4f2502ef13c4a27bba
Vinnie Marone <vinnie.marone35@gmail.com>
2022-08-08 17:27:40 -0400

    Add redis store

---
+++ store/store_service.go
@@ -0,0 +1,60 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v9"
+	"time"
+)
+
+// Define the struct wrapper around raw Redis client
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+// Top level declarations for the storeService and Redis context
+var (
+	storeService = &StorageService{}
+    ctx = context.Background()
+)
+
+// Note that in a real world usage, the cache duration shouldn't have
+// an expiration time, an LRU policy config should be set where the
+// values that are retrieved less often are purged automatically from
+// the cache and stored back in RDBMS whenever the cache is full
+
+const CacheDuration = 6 * time.Hour
+
+
+// Initializing the store service and return a store pointer
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
+
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return result
+}
